914736766d140adbad207a329bb50814851b6121
awanke <awanke@163.com>
2015-10-08 18:39:16 +0800

    m

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip install Django==1.8.4
+pip install django-suit==0.2.15
+pip install django-session-security
+pip install paramiko
+#安装python连接mysql的模块
+yum install MySQL-python  #for CentOS
+pip install MySQL-python  #for Ubuntu
